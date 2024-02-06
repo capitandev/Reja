800812fe80f1a8e87bc33db730db4db21e519887
capitandev <sultanovabulkhalik@gmail.com>
2024-02-06 14:38:42 +0900

    upload server.js

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
-console.log("Web server");
+console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
-const http = require("http"); 
+const http = require("http");
 
-//1 - Kirish code 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// 1: Kirish 
+app.use(express.static("public")); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
-//2 - Session Kodlar:
+// 2: Session code
 
-//3 - view code
-app.set("views","views");
-app.set("view engine","ejs");
+// 3: Views codetun
+app.set("views", "views"); 
+app.set("view engine", "ejs"); 
 
-//4 Rounting code
-app.post("/create-item", (req, res) =>{
-    console.log(req.body); // req ni faqta budy qismi uchun 
-    //console.log(req.); // reqesni malumtolarini hammsini ko'rish uchun 
-    res.json({test: "success"});
-
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  //console.log(req);
+  res.json({ test: "success" });
+});
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
-
-app.get("/", function(req, res) {
-    res.render("harid");
-});
-
-
-// app.get("/", function(req, res){
-//     res.end("<h1>salom b alex</h1>");
-// })
-// app.get("/gift", function(req, res){
-//     res.end("<h1>salom bu gift </h1>" );
-// })
-
-const server = http.createServer(app); 
-let Port = 3000;
-server.listen(Port, function() {
-    console.log(`The server is running successfully on port: ${Port}`);
+const server = http.createServer(app);
+let PORT = 3006;
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port: ${PORT}`);
 });
