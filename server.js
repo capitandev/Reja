ee38891acb98e6a61f742710768e0e05c82b2c7b
capitandev <sultanovabulkhalik@gmail.com>
2024-02-07 16:57:51 +0900

    Creat author.ejs & Fix: server.ejs and Creat portfolio

---
+++ server.js
@@ -0,0 +1,42 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1: Kirish code
+app.use(express.static("public")); // brouserlarga public folder ochiq degani.
+app.use(express.json()); // kirib kelayotgan data ni objectga ozgartirib beradi.
+app.use(express.urlencoded({ extended: true })); // html formdan kelgan ma'lumotlarni qabul qiladi.
+
+// 2: Session code
+
+// 3: Views code
+app.set("views", "views"); // folderni korsatyapmiz.
+app.set("view engine", "ejs"); // view engine ejs ekanligini korsatyapmiz.
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  res.json({ test: "success" });
+});
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
+app.get("/author", function (req, res) {
+    res.render("author", { user: user });
+  });
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port: ${PORT}`);
+});
