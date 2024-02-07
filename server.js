54c65f94ed04ada66eec3765a995fab7580405cd
Alex <66161421+capitandev@users.noreply.github.com>
2024-02-07 19:13:16 +0900

    Update server.js
    Signed-off-by: Alex <66161421+capitandev@users.noreply.github.com>

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ fs.readFile("database/user.json", "utf-8", (err, data) => {
   }
 });
 
-// 1: Kirish code
-app.use(express.static("public")); // brouserlarga public folder ochiq degani.
-app.use(express.json()); // kirib kelayotgan data ni objectga ozgartirib beradi.
-app.use(express.urlencoded({ extended: true })); // html formdan kelgan ma'lumotlarni qabul qiladi.
+// 1: Kirish cod
+app.use(express.static("public")); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
 
 // 2: Session code
 
 // 3: Views code
-app.set("views", "views"); // folderni korsatyapmiz.
-app.set("view engine", "ejs"); // view engine ejs ekanligini korsatyapmiz.
+app.set("views", "views"); .
+app.set("view engine", "ejs"); 
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
